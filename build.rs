1b11dd8161b7593fafb8e69c6e433f8966d8d797
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2025-12-01 13:41:36 -0800

    cargo update

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use tonic_build::compile_protos;
+use tonic_prost_build::compile_protos;
 
 fn main() -> Result<(), Box<dyn Error>> {
     compile_protos("proto/sni.proto")?;
